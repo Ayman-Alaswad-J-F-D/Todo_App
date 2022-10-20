4f8e4d19f11ca22a58ed4ef1e905780d9a1fe818
Ayman Alaswad <aymandeveloper0git@gmail.com>
2022-10-21 02:17:45 +0300

    Edit in the main file
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,7 @@ import 'package:todo_app/shared/bloc_observer.dart';
 
 import 'shared/cubit/cubit.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() {
   // ignore: deprecated_member_use
   BlocOverrides.runZoned(
     () {
@@ -20,14 +19,12 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => TodoAppCubit()..createDatabase(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.teal,
         ),
