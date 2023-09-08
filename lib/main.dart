c71318bb65b5009571a6d0009d619ff7cd4d5624
Ayman Alaswad <aymandeveloper0git@gmail.com>
2023-09-08 20:30:09 +0300

    Edit in code ( refatory and clean )
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
           fontFamily: AppFonts.primaryFont,
           primarySwatch: AppColors.primary,
         ),
-        home: HomeLayout(),
+        home: const HomeLayout(),
       ),
     );
   }
