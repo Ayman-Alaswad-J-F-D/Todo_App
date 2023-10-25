fd48ad01f28006adf7283fdefc2c3c0c0ed1b076
Ayman Alaswad <aymandeveloper0git@gmail.com>
2023-10-25 21:39:02 +0300

    Update UI and refactory code

--- lib/modules/archive_tasks/archive_tasks_screen.dart
+++ lib/modules/archive_tasks/archive_tasks_screen.dart
@@ -1,19 +1,42 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/shared/components/build_all_screen.dart';
+import 'package:todo_app/widgets/list_view_animation.dart';
 
-import '../../shared/components/tasks_builder.dart';
+import '../../shared/components/build_task_item.dart';
 import '../../shared/cubit/cubit.dart';
+import '../../widgets/custom_divider.dart';
+import '../../widgets/fall_back_widget.dart';
 
 class ArchiveTasksScreen extends StatelessWidget {
   const ArchiveTasksScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TodoAppCubit, TodoAppStates>(
-      builder: (context, state) {
-        var tasks = TodoAppCubit.get(context).archiveTasks;
-        return TasksBuilder(tasks: tasks, isDone: false);
-      },
+    return BuildViewAllTasks(
+      headerLabel: 'Archive Tasks',
+      phraseLabel: 'Keep Going🙏',
+      listViewItem: BlocBuilder<TodoAppCubit, TodoAppStates>(
+        builder: (context, state) {
+          final listArchive = TodoAppCubit.get(context).archiveTasks;
+
+          return ConditionalBuilder(
+            condition: state is! IsEmptyDatebaseState && listArchive.isNotEmpty,
+            builder: (context) => ListView.separated(
+              shrinkWrap: true,
+              padding: EdgeInsets.zero,
+              itemCount: listArchive.length,
+              itemBuilder: (context, index) => ListViewAnimation(
+                index: index,
+                child: BuildTaskItem(data: listArchive[index], index: index),
+              ),
+              separatorBuilder: (context, index) => CustomDivider(index: index),
+            ),
+            fallback: (context) => const FallBackWidget(),
+          );
+        },
+      ),
     );
   }
 }
