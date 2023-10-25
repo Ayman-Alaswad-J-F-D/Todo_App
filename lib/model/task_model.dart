209cbd18f4360cec45b8a2b6c729fc88b9c9dfe8
Ayman Alaswad <aymandeveloper0git@gmail.com>
2023-10-25 21:34:47 +0300

    Create class model to task data

---
+++ lib/model/task_model.dart
@@ -0,0 +1,38 @@
+class TaskModel {
+  int id;
+  String title, time, date, status, note, image;
+
+  TaskModel({
+    required this.id,
+    required this.title,
+    required this.time,
+    required this.date,
+    required this.status,
+    required this.note,
+    required this.image,
+  });
+
+  factory TaskModel.fromMap(Map<String, dynamic> item) {
+    return TaskModel(
+      id: item['id'],
+      title: item['title'],
+      time: item['time'],
+      date: item['date'],
+      status: item['status'],
+      note: item['note'],
+      image: item['image'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'time': time,
+      'date': date,
+      'status': status,
+      'note': note,
+      'image': image,
+    };
+  }
+}
