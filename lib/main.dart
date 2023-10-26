b2260f17785b9fd559bd3993cf79312a28992741
Ayman Alaswad <aymandeveloper0git@gmail.com>
2023-10-26 10:28:42 +0300

    Add ScreenUtilInit widget for Responsive width and height and font size text in screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:todo_app/modules/splash_screen/splash_screen.dart';
+import 'package:todo_app/shared/styles/colors.dart';
 
-import 'Layout/Home/home_layout.dart';
 import 'shared/bloc_observer.dart';
 import 'shared/cubit/cubit.dart';
-import 'shared/styles/colors.dart';
-import 'shared/styles/styles.dart';
 
 void main() {
   Bloc.observer = MyBlocObserver();
-  runApp(const MyApp());
+  runApp(const TodoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class TodoApp extends StatelessWidget {
+  const TodoApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => TodoAppCubit()..createDatabase(),
-      child: MaterialApp(
-        navigatorKey: GlobalKey<NavigatorState>(),
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: AppFonts.primaryFont,
-          primarySwatch: AppColors.primary,
-        ),
-        home: const HomeLayout(),
-      ),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return BlocProvider(
+          create: (context) => TodoAppCubit()..createDatabase(),
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              // fontFamily: AppFonts.primaryFont,
+              primarySwatch: AppColors.primary,
+              bottomSheetTheme: const BottomSheetThemeData(
+                backgroundColor: Colors.transparent,
+              ),
+            ),
+            home: child,
+          ),
+        );
+      },
+      child: const SplashScreen(),
     );
   }
 }
