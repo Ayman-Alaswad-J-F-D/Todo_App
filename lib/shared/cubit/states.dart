9c4e54c7242ba8d161c80748e873bdbe989fc9bf
Ayman Alaswad <aymandeveloper0git@gmail.com>
2023-10-27 07:41:44 +0200

    Delete some unnecessary states

--- lib/shared/cubit/states.dart
+++ lib/shared/cubit/states.dart
@@ -16,10 +16,6 @@ class IsEmptyDatebaseState extends TodoAppStates {}
 
 class InsertDatabaseState extends TodoAppStates {}
 
-class ChangeBottomSheetOpenState extends TodoAppStates {}
-
-class ChangeBottomSheetCloseState extends TodoAppStates {}
-
 class UpdateDatabaseState extends TodoAppStates {}
 
 class DeleteDatabaseState extends TodoAppStates {}
