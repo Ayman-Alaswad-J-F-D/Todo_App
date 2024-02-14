28d2589aa716a41aeb4525c5e59c9283486eaa12
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-02-14 17:11:27 +0300

    Adding the 'sort click' and 'delete click' arguments
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

--- lib/shared/components/build_all_screen.dart
+++ lib/shared/components/build_all_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:todo_app/shared/cubit/cubit.dart';
-
 import 'package:todo_app/shared/styles/colors.dart';
 
+import '../../widgets/confirm_dialog.dart';
 import 'background_header.dart';
 
 class BuildViewAllTasks extends StatelessWidget {
@@ -11,11 +11,15 @@ class BuildViewAllTasks extends StatelessWidget {
     Key? key,
     required this.headerLabel,
     required this.phraseLabel,
+    required this.sortClick,
+    required this.deleteClick,
     required this.listViewItem,
   }) : super(key: key);
 
   final String headerLabel;
   final String phraseLabel;
+  final VoidCallback sortClick;
+  final VoidCallback deleteClick;
   final Widget listViewItem;
 
   @override
@@ -27,6 +31,7 @@ class BuildViewAllTasks extends StatelessWidget {
         children: [
           Expanded(
             child: BackgroundHeader(
+              positionedTop: 15,
               headerLabel: headerLabel,
               leadingIcon: Icons.arrow_back_ios_new_rounded,
               leadingClick: () => Navigator.pop(context),
@@ -40,7 +45,21 @@ class BuildViewAllTasks extends StatelessWidget {
                 if (cubit.archiveTasks.isNotEmpty ||
                     cubit.doneTasks.isNotEmpty ||
                     cubit.newTasks.isNotEmpty)
-                  PhraseLabel(titleBody: phraseLabel),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 4.0,
+                      left: 12.0,
+                      right: 6.0,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        PhraseLabel(titleBody: phraseLabel),
+                        DeleteButton(deleteClick: deleteClick),
+                        SortButton(sortClick: sortClick),
+                      ],
+                    ),
+                  ),
 
                 //* The Tasks List
                 Expanded(child: listViewItem),
@@ -64,11 +83,8 @@ class PhraseLabel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 16,
-        vertical: 12,
-      ),
+    return Expanded(
+      flex: 5,
       child: RichText(
         text: TextSpan(
           text: 'Todo :  ',
@@ -97,6 +113,60 @@ class PhraseLabel extends StatelessWidget {
             TextSpan(text: '  ${titleBody.characters.last}'),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SortButton extends StatelessWidget {
+  const SortButton({
+    Key? key,
+    required this.sortClick,
+  }) : super(key: key);
+
+  final VoidCallback sortClick;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Transform.flip(
+        flipX: true,
+        child: IconButton(
+          icon: const Icon(Icons.format_line_spacing_rounded),
+          onPressed: sortClick,
+          color: AppColors.primary,
+          tooltip: 'Sort Tasks',
+        ),
+      ),
+    );
+  }
+}
+
+class DeleteButton extends StatelessWidget {
+  const DeleteButton({
+    Key? key,
+    required this.deleteClick,
+  }) : super(key: key);
+
+  final VoidCallback deleteClick;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: IconButton(
+        icon: const Icon(Icons.delete),
+        onPressed: () => confirmDialog(
+          context: context,
+          titleDialog: "Delete Confirmation",
+          questionDialog: "Are you sure you want to delete all this tasks ?",
+          labelButton: "Delete All",
+          color: AppColors.red,
+        ).then((isDeleted) {
+          if (isDeleted) return deleteClick();
+        }),
+        color: AppColors.red,
+        iconSize: 22,
+        tooltip: 'Delete All Tasks',
       ),
     );
   }
