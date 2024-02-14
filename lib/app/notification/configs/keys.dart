02140f2f38fc13d2b1c56882c3723f52b7d07f0f
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-02-14 15:15:11 +0300

    Keys for channels and other configurations for local notifications
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

---
+++ lib/app/notification/configs/keys.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: unused_element, constant_identifier_names
+
+const ICON_APP_NOTIFICATION_ = '@mipmap/ic_launcher';
+
+const ticker = 'ticker';
+const channel_id = 'channel_id';
+const channel_title = 'channel_title';
+const channel_description = 'channel_description';
+
+const channel_group_id = 'channel_id_group';
+const channel_group_title = 'channel_title_group';
+const group_key = 'group_key';
+const group_id = 0;
