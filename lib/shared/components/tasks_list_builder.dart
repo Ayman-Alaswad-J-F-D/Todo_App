8722ad4c14b5dd31a996df5f39e20f7cf5a37fb4
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-02-14 17:13:48 +0300

    Adding a style to the TextButton
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

--- lib/shared/components/tasks_list_builder.dart
+++ lib/shared/components/tasks_list_builder.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:todo_app/model/task_model.dart';
 import 'package:todo_app/modules/all_tasks_screen/all_tasks_screen.dart';
 import 'package:todo_app/modules/done_tasks/done_tasks_screen.dart';
@@ -21,60 +20,49 @@ class TasksListBuilder extends StatelessWidget {
 
   final List<TaskModel> tasks;
   final bool isDone;
-  // final bool isArchive;
 
   int _getLength() {
     return isDone
         ? (tasks.length >= 2 ? 2 : tasks.length)
-        : (tasks.length >= 4 ? 4 : tasks.length);
+        : (tasks.length > 4 ? 4 : tasks.length);
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      // crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         Expanded(
+          flex: isDone ? 6 : 7,
           child: ListView.separated(
             // reverse: true,
             shrinkWrap: true,
             padding: EdgeInsets.zero,
             itemCount: _getLength(),
-            physics: const BouncingScrollPhysics(),
             itemBuilder: (context, index) => ListViewAnimation(
               index: index,
               delay: 800,
               verticalOffset: false,
               horizontalOffset: widthScreen(context) - 30,
-              child: BuildTaskItem(
-                data: tasks[index],
-                isDone: isDone,
-                index: index,
-              ),
+              child: BuildTaskItem(data: tasks[index], isDone: isDone),
             ),
             separatorBuilder: (context, index) => CustomDivider(
               index: index,
-              delayAnimation: 800,
+              delayAnimation: 600,
               verticalOffsetAnimation: false,
             ),
           ),
         ),
         if (tasks.length >= 3)
-          Container(
-            height: 26.h,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: AppColors.primary.shade100,
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 1,
-                  spreadRadius: 1,
-                  offset: Offset(0, -1),
-                ),
-              ],
-            ),
+          Expanded(
             child: TextButton(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              style: TextButton.styleFrom(
+                elevation: 12,
+                padding: EdgeInsets.zero,
+                backgroundColor: AppColors.primary.shade100,
+                minimumSize: const Size(double.maxFinite, 20),
+                maximumSize: const Size(double.maxFinite, 30),
+              ),
               child: const LabelWidget(
                 title: 'See All',
                 fontSize: 11,
