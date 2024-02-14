4786dd208cc34e8eee957c0ea8b89f496f6e47ba
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-02-14 17:15:27 +0300

    File for database column names and title nottification
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

---
+++ lib/shared/constants/strings.dart
@@ -0,0 +1,14 @@
+//* A title for all notifications
+const String titleNotifications = "Todo Day";
+//* Table name locla storage
+const String tabelTodo = 'tasks';
+const String columnId = 'id';
+const String columnTitle = 'title';
+const String columnTime = 'time';
+const String columnDate = 'date';
+const String columnStatus = 'status';
+const String columnNote = 'note';
+const String columnImage = 'image';
+const String columnIsArchive = 'isArchive';
+const String statusTask = 'new';
+const int isArchive = 0;
