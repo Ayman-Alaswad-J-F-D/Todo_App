0b53503d974b811db3299e055e02680f0daf4169
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-02-14 17:14:35 +0300

    Changing the name of images
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

--- lib/shared/constants/constants.dart
+++ lib/shared/constants/constants.dart
@@ -9,27 +9,14 @@ class Constants {
   static const String appIcon = '$assets/app_icon/icon.png';
   static const String splashIcon = '$assets/splash_icon.png';
 
-  static const String categoryEvent = '$imagePath/categoryEvent.svg';
-  static const String categoryGoal = '$imagePath/categoryGoal.svg';
-  static const String categoryTask = '$imagePath/categoryTask.svg';
+  static const String categoryEventIcon = '$imagePath/category_event.svg';
+  static const String categoryGoalIcon = '$imagePath/category_goal.svg';
+  static const String categoryTaskIcon = '$imagePath/category_task.svg';
 
   static const List<String> listSvgIcons = [
-    categoryTask,
-    categoryGoal,
-    categoryEvent,
-  ];
-
-  static const List<String> assetsImage = [
-    '$assets/eat_image.png',
-    '$assets/play_image.png',
-    '$assets/watching_image.png',
-    '$assets/sport_image.png',
-    '$assets/code_image.png',
-    '$assets/driving_image.png',
-    '$assets/tool-image.png',
-    '$assets/pencil_image.png',
-    '$assets/work_image.png',
-    '$assets/plane_image.png',
+    categoryTaskIcon,
+    categoryGoalIcon,
+    categoryEventIcon,
   ];
 }
 
