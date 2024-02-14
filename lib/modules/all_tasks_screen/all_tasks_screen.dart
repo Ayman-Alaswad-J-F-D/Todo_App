4278ac06d4e8328d953d7a53cc9a26942788a65d
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-02-14 17:07:07 +0300

    Adding sort button and delete button and identify states when building
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

--- lib/modules/all_tasks_screen/all_tasks_screen.dart
+++ lib/modules/all_tasks_screen/all_tasks_screen.dart
@@ -2,36 +2,62 @@ import 'package:conditional_builder_null_safety/conditional_builder_null_safety.
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/shared/components/build_all_screen.dart';
-import 'package:todo_app/shared/constants/constants.dart';
 import 'package:todo_app/widgets/fall_back_widget.dart';
 import 'package:todo_app/widgets/list_view_animation.dart';
 
+import '../../app/global/global.dart';
 import '../../shared/components/build_task_item.dart';
 import '../../shared/cubit/cubit.dart';
 import '../../widgets/custom_divider.dart';
 
-class AllTasksScreen extends StatelessWidget {
+class AllTasksScreen extends StatefulWidget {
   const AllTasksScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AllTasksScreen> createState() => _AllTasksScreenState();
+}
+
+class _AllTasksScreenState extends State<AllTasksScreen> {
+  late TodoAppCubit cubit;
+
+  @override
+  void initState() {
+    _setup();
+    super.initState();
+  }
+
+  void _setup() {
+    cubit = TodoAppCubit.get(context);
+    cubit.cancelGroupNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
     return BuildViewAllTasks(
-      headerLabel: 'All Tasks',
+      headerLabel: 'All New Tasks',
       phraseLabel: 'Don\'t Give Up🤩',
+      sortClick: () => cubit.sortNewTasks(),
+      deleteClick: () =>
+          cubit.deleteAllTasksWhereStatus(status: Global.isNewTask),
       listViewItem: BlocBuilder<TodoAppCubit, TodoAppStates>(
+        buildWhen: (_, current) =>
+            current is CreateDatabaseState ||
+            current is UpdateDatabaseState ||
+            current is DeleteDatabaseState ||
+            current is SortingListState,
         builder: (context, state) {
-          final listNew = TodoAppCubit.get(context).newTasks;
+          final listNewTasks = cubit.newTasks;
 
           return ConditionalBuilder(
-            condition: state is! IsEmptyDatebaseState && listNew.isNotEmpty,
+            condition:
+                state is! IsEmptyDatebaseState && listNewTasks.isNotEmpty,
             builder: (context) => ListView.separated(
               shrinkWrap: true,
               padding: EdgeInsets.zero,
-              itemCount: listNew.length,
+              itemCount: listNewTasks.length,
               itemBuilder: (context, index) => ListViewAnimation(
                 index: index,
-                horizontalOffset: widthScreen(context),
-                child: BuildTaskItem(data: listNew[index], index: index),
+                child: BuildTaskItem(data: listNewTasks[index]),
               ),
               separatorBuilder: (context, index) => CustomDivider(index: index),
             ),
