565685edd9a108050102dd6ec8f0560b1267a3d4
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-02-14 17:20:15 +0300

    Light theme app
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

---
+++ lib/shared/theme/light_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import '../styles/colors.dart';
+
+ThemeData lightTheme = ThemeData(
+  // fontFamily: AppFonts.primaryFont,
+  primarySwatch: AppColors.primary,
+  bottomSheetTheme: const BottomSheetThemeData(
+    backgroundColor: Colors.transparent,
+  ),
+);
