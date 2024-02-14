d72dfb896129f0a0851beeabbbbf993fa9510768
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-02-14 17:18:03 +0300

    Fix image display
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

--- lib/shared/function/check_image.dart
+++ lib/shared/function/check_image.dart
@@ -1,17 +1,26 @@
-import 'dart:io';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:path/path.dart' as p;
 
-Widget checkImage({required image, double? width = 40}) {
-  return p.extension(image) == '.png' ||
-          p.extension(image) == '.jpg' ||
-          p.extension(image) == '.jpeg'
-      ? CircleAvatar(
-          backgroundImage: FileImage(File(image)),
-          radius: width != null ? (width.w / 2) : 24.r,
-        )
-      : SvgPicture.asset(image, width: width?.w);
+Widget checkImage({required String image, double? width = 40}) {
+  return p.extension(image) == '.svg'
+      ? SvgPicture.asset(image, width: width?.w)
+      : _circleImage(image, width);
+}
+
+Widget _circleImage(image, double? width) {
+  final bytes = base64Decode(image);
+  return CircleAvatar(
+    radius: width != null ? (width.w / 2) : 24.r,
+    child: Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(shape: BoxShape.circle),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Image.memory(bytes, fit: BoxFit.cover),
+    ),
+  );
 }
