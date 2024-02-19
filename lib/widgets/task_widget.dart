4d314d2da3d279d82866b198d24005cb112a81c6
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-02-19 08:44:53 +0300

    Pass the context to check the image and make the widget opacity in whether the task has been done or not.
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

--- lib/widgets/task_widget.dart
+++ lib/widgets/task_widget.dart
@@ -3,8 +3,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:todo_app/shared/function/check_image.dart';
 
 import 'custom_check_box.dart';
-import 'time_task.dart';
 import 'label_widget.dart';
+import 'time_task.dart';
 
 class TaskWidget extends StatelessWidget {
   const TaskWidget({
@@ -14,14 +14,14 @@ class TaskWidget extends StatelessWidget {
     required this.image,
     required this.checkBoxValue,
     required this.onChange,
-    this.opacity = 1,
+    this.isDoneTask = false,
     this.decoration,
   }) : super(key: key);
 
   final String taskTitle;
   final String taskTime;
   final String image;
-  final double opacity;
+  final bool isDoneTask;
   final TextDecoration? decoration;
   final Function(bool?) onChange;
   final bool checkBoxValue;
@@ -30,13 +30,13 @@ class TaskWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       child: Opacity(
-        opacity: opacity,
+        opacity: isDoneTask ? .4 : 1,
         child: ListTile(
           enabled: false,
           contentPadding:
               const EdgeInsets.only(left: 14, bottom: 5, top: 5, right: 10),
           minLeadingWidth: 30.w,
-          leading: checkImage(image: image),
+          leading: checkImage(context, image: image),
           title: LabelWidget(
             title: taskTitle,
             fontWeight: FontWeight.bold,
