904d2e274362d684f90e0c008acf1350eb25c700
Ayman Alaswad <aymandeveloper0git@gmail.com>
2024-02-19 15:57:15 +0300

    The context argument was added to calculate the height of the cache in order to better display the image
    Signed-off-by: Ayman Alaswad <aymandeveloper0git@gmail.com>

--- lib/shared/function/check_image.dart
+++ lib/shared/function/check_image.dart
@@ -3,24 +3,28 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:path/path.dart' as p;
 
-Widget checkImage({required String image, double? width = 40}) {
-  return p.extension(image) == '.svg'
-      ? SvgPicture.asset(image, width: width?.w)
-      : _circleImage(image, width);
+Widget checkImage(context, {required String image, double width = 42}) {
+  return image.split('.').last == 'svg'
+      ? SvgPicture.asset(image, width: width.w)
+      : _circleImage(context, image, width);
 }
 
-Widget _circleImage(image, double? width) {
+Widget _circleImage(BuildContext context, String image, double width) {
   final bytes = base64Decode(image);
   return CircleAvatar(
-    radius: width != null ? (width.w / 2) : 24.r,
+    radius: (width / 2).r,
     child: Container(
       width: double.infinity,
       height: double.infinity,
       decoration: const BoxDecoration(shape: BoxShape.circle),
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      child: Image.memory(bytes, fit: BoxFit.cover),
+      child: Image.memory(
+        bytes,
+        height: 150.h,
+        fit: BoxFit.cover,
+        cacheHeight: (150 * MediaQuery.of(context).devicePixelRatio).round(),
+      ),
     ),
   );
 }
